bcaf892e1c41d638377e2d87f383f50d4537a06e
luyilin <luyilin12@gmail.com>
2018-11-15 14:03:18 +0800

    update: remove disqusjs temporarily

--- themes/layouts/comment.vue
+++ themes/layouts/comment.vue
@@ -12,24 +12,13 @@
 <script>
 import Main from './main'
 import Markdown from '@theme/components/Markdown'
-import 'disqusjs/dist/disqusjs.css'
-// import DisqusJS from 'disqusjs'
 
 let loadDisqus = () => {
   (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://cdn.jsdelivr.net/npm/disqusjs@1.0.7/dist/disqus.js';
+    s.src = 'https://minemine-2.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
-    setTimeout(() => {
-      var dsqjs = new DisqusJS({
-        shortname: 'mine',
-        siteName: "mine",
-        url: "https://minemine.cc",
-        apikey: '6hsuNYhuAn3K2X6Jtal9IHwLKG29LnCCsLI49Q5f4dPffLoSfthM5DkaE2Jilxex',
-        admin: 'luyilin'
-      });
-    }, 500)
   })()
 }
 
