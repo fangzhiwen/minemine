082ee805fe7c40d2dd68df138c2946d808a14ccf
luyilin <luyilin12@gmail.com>
2018-11-15 12:57:05 +0800

    feat: use disqusjs

--- themes/layouts/comment.vue
+++ themes/layouts/comment.vue
@@ -13,13 +13,12 @@
 import Main from './main'
 import Markdown from '@theme/components/Markdown'
 import 'disqusjs/dist/disqusjs.css'
-import DisqusJS from 'disqusjs'
+// import DisqusJS from 'disqusjs'
 
 let loadDisqus = () => {
   (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    // s.src = 'https://unpkg.com/disqusjs@1.0/dist/disqus.js';
-    s.src = 'https://minemine-2.disqus.com/embed.js';
+    s.src = 'https://cdn.jsdelivr.net/npm/disqusjs@1.0.7/dist/disqus.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
   })()
@@ -35,16 +34,12 @@ export default {
 
   mounted() {
     loadDisqus()
-    console.log(DisqusJS)
     var dsqjs = new DisqusJS({
-      shortname: 'minemine-2',
-      siteName: 'minemine',
-      identifier: '',
-      url: '',
-      api: '',
-      apikey: '',
-      admin: '',
-      adminLabel: ''
+      shortname: 'mine',
+      siteName: "mine",
+      url: "https://minemine.cc",
+      apikey: '6hsuNYhuAn3K2X6Jtal9IHwLKG29LnCCsLI49Q5f4dPffLoSfthM5DkaE2Jilxex',
+      admin: 'luyilin'
     });
   },
 
