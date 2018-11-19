205b2ff5d790b8011341dbb2b135d93cde2318bb
luyilin <luyilin12@gmail.com>
2018-11-19 19:08:33 +0800

    fix: use location url

--- themes/components/Disqus.vue
+++ themes/components/Disqus.vue
@@ -11,7 +11,6 @@ export default {
     var dsqjs = new DisqusJS({
       shortname: 'minemine',
       siteName: "minemine",
-      url: "https://minemine.cc",
       apikey: '6hsuNYhuAn3K2X6Jtal9IHwLKG29LnCCsLI49Q5f4dPffLoSfthM5DkaE2Jilxex',
       admin: 'luyilin'
     });
